e303bb87f247c71534e5aa8ae96f8cb6dc5ebea3
SohelDewan <sohelds241@gmail.com>
2024-05-28 14:27:08 +0600

    payment method added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // middleware
 app.use(cors());
@@ -31,6 +32,7 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
+    const paymentCollection = client.db("bistroDb").collection("payment");
 
     // jwt related api
     app.post('/jwt', async (req, res) => {
@@ -185,7 +187,33 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
+    // payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "usd",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    payment_method_types:['card']
+  });
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    })
 
+})
+  app.post('/payments', async (req, res) => {
+    const payment = req.body;
+    const paymentResult = await paymentCollection.insertOne(payment)
+    // carefully delete each item from the cart
+    console.log('payment info',payment);
+    const query = {_id:{
+      $in: payment.cartIds.map(id => new ObjectId(id))
+    }}
+    const deleteResult = await cartCollection.deleteMany(query);
+    res.send({paymentResult, deleteResult});
+  })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
